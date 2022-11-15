26dc251eeae0c772180268dcc7f462d8c4fee8ea
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-11-15 13:21:00 +0100

    GPU friendly & cosmetic changes

--- GeoParams_PT_examples/scripts/Stokes2D_VEP_reg_ctau_inv_GeoParams_dampV3_ps.jl
+++ GeoParams_PT_examples/scripts/Stokes2D_VEP_reg_ctau_inv_GeoParams_dampV3_ps.jl
@@ -1,24 +1,17 @@
 using ElasticArrays, Printf, GeoParams
 using Plots, Plots.Measures
-
 using ParallelStencil
 using ParallelStencil.FiniteDifferences2D
-@init_parallel_stencil(Threads, Float64, 2)
+@init_parallel_stencil(CUDA, Float64, 2)
 
 plot_opts() = (aspect_ratio = 1, xlims = (0.007936507936507936, 0.9920634920634921), ylims = (0.007936507936507936, 0.9920634920634921), c = cgrad(:roma, rev=true), framestyle = :box)
 
-@parallel function update_old!(Txx_o, Tyy_o, Txy_o, Txyv_o, Pr_old, Txx, Tyy, Txy, Txyv, Pr)
-    @all(Txx_o) = @all(Txx)
-    @all(Tyy_o) = @all(Tyy)
-    @all(Txy_o) = @all(Txy)
-    @all(Txyv_o) = @all(Txyv)
-    @all(Pr_old) = @all(Pr)
-    return nothing
-end
-
-@views function update_iteration_params!(η, ητ)
-    ητ[2:(end - 1), 2:(end - 1)] .= maxloc(η)
-    bc2!(ητ)
+@parallel function update_old!(τxx_o, Tyy_o, τxy_o, τxyv_o, Pt_old, τxx, τyy, τxy, τxyv, Pt)
+    @all(τxx_o) = @all(τxx)
+    @all(Tyy_o) = @all(τyy)
+    @all(τxy_o) = @all(τxy)
+    @all(τxyv_o) = @all(τxyv)
+    @all(Pt_old) = @all(Pt)
     return nothing
 end
 
@@ -63,7 +56,7 @@ end
     return nothing
 end
 
-@parallel_indices (i, j) function update_stress_GP_ps!(Txx, Tyy, Txy, TII, Txx_o, Tyy_o, Txyv_o, Exx, Eyy, Exyv, η_vep, Pt, Phasec, Phasev, MatParam, dt, lτ, r, re_mech, vdτ)
+@parallel_indices (i, j) function update_stress_GP_ps!(τxx, τyy, τxy, TII, τxx_o, Tyy_o, τxyv_o, εxx, εyy, εxyv, η_vep, Pt, Phasec, Phasev, MatParam, dt, lτ, r, re_mech, vdτ)
 
     # convinience closure (note here that i, j are captured because the closure is defined inside the loop when @parallel_indices is expanded)
     @inbounds @inline gather(A) = A[i, j], A[i + 1, j], A[i, j + 1], A[i + 1, j + 1] 
@@ -73,14 +66,14 @@ end
     dτ_r                = 1.0 / (θ_dτ + 1 / η_vep[i, j]) # equivalent to dτ_r = @. 1.0/(θ_dτ + η/(G*dt) + 1.0)
     # Setup up input for GeoParams.jl
     args                = (; dt=dt, P=Pt[i, j], τII_old=0.0)
-    εij_p               = (Exx[i, j], Eyy[i, j], gather(Exyv))
-    τij_p_o             = (Txx_o[i,j], Tyy_o[i,j], gather(Txyv_o))
+    εij_p               = (εxx[i, j], εyy[i, j], gather(εxyv))
+    τij_p_o             = (τxx_o[i,j], Tyy_o[i,j], gather(τxyv_o))
     phases              = (Phasec[i, j], Phasec[i, j], gather(Phasev))
     # update stress and effective viscosity
     Tij, TII[i, j],  ηᵢ = compute_τij(MatParam, εij_p, args, τij_p_o, phases)
-    Txx[i,j]           += dτ_r * (-(Txx[i,j]) + Tij[1] ) /  ηᵢ # NOTE: from GP Tij = 2*η_vep * εij
-    Tyy[i,j]           += dτ_r * (-(Tyy[i,j]) + Tij[2] ) /  ηᵢ 
-    Txy[i,j]           += dτ_r * (-(Txy[i,j]) + Tij[3] ) /  ηᵢ 
+    τxx[i,j]           += dτ_r * (-(τxx[i,j]) + Tij[1] ) /  ηᵢ # NOTE: from GP Tij = 2*η_vep * εij
+    τyy[i,j]           += dτ_r * (-(τyy[i,j]) + Tij[2] ) /  ηᵢ 
+    τxy[i,j]           += dτ_r * (-(τxy[i,j]) + Tij[3] ) /  ηᵢ 
     η_vep[i, j]         =  ηᵢ
 
     return
@@ -101,8 +94,9 @@ end
     return nothing
 end
 
-@parallel function compute_P!(P, P_old, ∇V, η, K, dt, r, θ_dτ)
-    @all(P) = @all(P) + (-@all(∇V) - (@all(P) - @all(P_old)) / (@all(K) * dt)) / (1.0 / (r / θ_dτ * @all(η)) + 1.0 /(@all(K) * dt))
+@parallel function compute_P!(P, P_old, RP, ∇V, η, K, dt, r, θ_dτ)
+    @all(RP) = (-@all(∇V) - (@all(P) - @all(P_old)) / (@all(K) * dt))
+    @all(P)  = @all(P) + (-@all(∇V) - (@all(P) - @all(P_old)) / (@all(K) * dt)) / (1.0 / (r / θ_dτ * @all(η)) + 1.0 /(@all(K) * dt))
     return nothing
 end
 
@@ -171,17 +165,11 @@ function main(nt)
         ),
     )
     # Numerics
-    nx, ny  = 63, 63             # numerical grid resolution
-    Vdmp    = 4.0                # convergence acceleration (damping)
-    Vsc     = 2.0                # iterative time step limiter
-    Ptsc    = 6.0                # iterative time step limiter
-    ε       = 1e-6               # nonlinear tolerence
-    iterMax = 3e4                # max number of iters
-    nout    = 200                # check frequency
+    nx, ny  = 255, 255             # numerical grid resolution
     re_mech = 3π
     lτ      = min(Lx, Ly)
     dx, dy  = Lx / nx, Ly / ny
-    vdτ     = min(dx, dy) / √2.0 #* 0.5 
+    vdτ     = min(dx, dy) / √2.1 #* 0.5 
     r       = 0.7
     θ_dτ    = lτ * (r + 2.0) / (re_mech * vdτ)
     ηdτ     = vdτ * lτ / re_mech
@@ -189,48 +177,45 @@ function main(nt)
     dx, dy  = Lx / nx, Ly / ny
     dt      = μ0 / G0 / 4.0 # assumes Maxwell time of 4
     # Array initialisation
-    Pt      = zeros(nx, ny)
-    dPt     = zeros(nx, ny)
-    Pt_old  = zeros(nx, ny)
-    ∇V      = zeros(nx, ny)
-    Vx      = zeros(nx + 1, ny)
-    Vy      = zeros(nx, ny + 1)
-    Exx     = zeros(nx, ny)
-    Eyy     = zeros(nx, ny)
-    Exy     = zeros(nx, ny)
-    Exyv    = zeros(nx + 1, ny + 1)
-    Txx     = zeros(nx, ny)
-    Tyy     = zeros(nx, ny)
-    Txy     = zeros(nx, ny)
-    Txyv    = zeros(nx + 1, ny + 1)
-    Txx_o   = zeros(nx, ny)
-    Tyy_o   = zeros(nx, ny)
-    Txy_o   = zeros(nx, ny)
-    Txyv_o  = zeros(nx + 1, ny + 1)
-    TII     = zeros(nx, ny)
-    Rx      = zeros(nx - 1, ny - 2)
-    Ry      = zeros(nx - 2, ny - 1)
-    η_vep   = ones(nx, ny)
-    η       = ones(nx, ny)
-    ητ      = zeros(nx, ny)
-    Phasec  = ones(Int, nx, ny)
-    Phasev  = ones(Int, nx + 1, ny + 1)
-    ρgx     = zeros(nx - 1, ny    )
-    ρgy     = zeros(nx    , ny - 1)
-    Kb  = fill(Inf, nx, ny)
+    Pt      = @zeros(nx, ny)
+    Pt_old  = @zeros(nx, ny)
+    RP      = @zeros(nx, ny)
+    ∇V      = @zeros(nx, ny)
+    Vx      = @zeros(nx + 1, ny)
+    Vy      = @zeros(nx, ny + 1)
+    εxx     = @zeros(nx, ny)
+    εyy     = @zeros(nx, ny)
+    Exy     = @zeros(nx, ny)
+    εxyv    = @zeros(nx + 1, ny + 1)
+    τxx     = @zeros(nx, ny)
+    τyy     = @zeros(nx, ny)
+    τxy     = @zeros(nx, ny)
+    τxyv    = @zeros(nx + 1, ny + 1)
+    τxx_o   = @zeros(nx, ny)
+    Tyy_o   = @zeros(nx, ny)
+    τxy_o   = @zeros(nx, ny)
+    τxyv_o  = @zeros(nx + 1, ny + 1)
+    TII     = @zeros(nx, ny)
+    Rx      = @zeros(nx - 1, ny - 2)
+    Ry      = @zeros(nx - 2, ny - 1)
+    η_vep   = @ones(nx, ny)
+    η       = @ones(nx, ny)
+    ητ      = @zeros(nx, ny)
+    Phasec  = Int.(@ones(nx, ny))
+    Phasev  = Int.(@ones(nx + 1, ny + 1))
+    ρgx     = @zeros(nx - 1, ny    )
+    ρgy     = @zeros(nx    , ny - 1)
+    Kb      = @fill(Inf, nx, ny)
     # Initial condition
     xc, yc                = LinRange(dx / 2, Lx - dx / 2, nx), LinRange(dy / 2, Ly - dy / 2, ny)
     xc, yc                = LinRange(dx / 2, Lx - dx / 2, nx), LinRange(dy / 2, Ly - dy / 2, ny)
     xv, yv                = LinRange(0.0, Lx, nx + 1), LinRange(0.0, Ly, ny + 1)
-    Xvx                   = [x for x in xv, y in yc]
-    Yvy                   = [y for x in xc, y in yv]
+    Xvx                   = Data.Array([x for x in xv, y in yc])
+    Yvy                   = Data.Array([y for x in xc, y in yv])
     radc                  = (xc .- Lx ./ 2) .^ 2 .+ (yc' .- Ly ./ 2) .^ 2
     radv                  = (xv .- Lx ./ 2) .^ 2 .+ (yv' .- Ly ./ 2) .^ 2
-    η_e[radc .< radi]    .= dt * Gi
-    η_ev[radv .< radi]   .= dt * Gi
     Phasec[radc .< radi] .= 2
     Phasev[radv .< radi] .= 2
-    η_vep                .= (1.0 ./ η_e + 1.0 ./ η_v) .^ -1
     Vx                   .=   εbg .* Xvx
     Vy                   .= .-εbg .* Yvy
     # Time loop
@@ -245,29 +230,28 @@ function main(nt)
     errs_evo = ElasticMatrix{Float64}(undef, length(ϵtol), 0)
     for it in 1:nt
         @printf("it=%d\n", it)
-        @parallel update_old!(Txx_o, Tyy_o, Txy_o, Txyv_o, Pt_old, Txx, Tyy, Txy, Txyv, Pt)
+        @parallel update_old!(τxx_o, Tyy_o, τxy_o, τxyv_o, Pt_old, τxx, τyy, τxy, τxyv, Pt)
         errs = 2.0 .* ϵtol
         iter = 1
         # resize!(iter_evo, 0)
         resize!(errs_evo, length(ϵtol), 0)
         while any(errs .>= ϵtol) && iter <= maxiter
-            # update_iteration_params!(η, ητ)
             @parallel (2:nx-1, 2:ny-1) update_iteration_params!(η, ητ)
             bc2!(ητ)
 
             @parallel compute_∇V!(∇V, Vx, Vy, dx, dy) 
-            @parallel compute_P!(Pt, Pt_old, ∇V, η, Kb, dt, r, θ_dτ)
-            @parallel compute_strain_rate!(∇V, Exx, Eyy, Exyv, Vx, Vy, dx, dy)
-            @parallel vertex2center!(Exy, Exyv)
-            @parallel (1:nx, 1:ny) update_stress_GP_ps!(Txx, Tyy, Txy, TII, Txx_o, Tyy_o, Txyv_o, Exx, Eyy, Exyv, η_vep, Pt, Phasec, Phasev, MatParam, dt, lτ, r, re_mech, vdτ)
-            @parallel center2vertex!(Txyv, Txy)
+            @parallel compute_P!(Pt, Pt_old, RP, ∇V, η, Kb, dt, r, θ_dτ)
+            @parallel compute_strain_rate!(∇V, εxx, εyy, εxyv, Vx, Vy, dx, dy)
+            @parallel vertex2center!(Exy, εxyv)
+            @parallel (1:nx, 1:ny) update_stress_GP_ps!(τxx, τyy, τxy, TII, τxx_o, Tyy_o, τxyv_o, εxx, εyy, εxyv, η_vep, Pt, Phasec, Phasev, MatParam, dt, lτ, r, re_mech, vdτ)
+            @parallel center2vertex!(τxyv, τxy)
 
-            @parallel (1:nx, 1:ny) update_velocities!(Vx, Vy, Pt, Txx, Tyy, Txyv, ηdτ, ρgx, ρgy, ητ, dx, dy)
+            @parallel (1:nx, 1:ny) update_velocities!(Vx, Vy, Pt, τxx, τyy, τxyv, ηdτ, ρgx, ρgy, ητ, dx, dy)
 
             if iter % ncheck == 0
                 # update residuals
-                @parallel compute_residuals!(Rx, Ry, Pt, Txx, Tyy, Txyv, ρgx, ρgy, dx, dy)
-                errs = max(abs.(Rx), abs.(Ry), abs.(dPt))
+                @parallel compute_residuals!(Rx, Ry, Pt, τxx, τyy, τxyv, ρgx, ρgy, dx, dy)
+                errs = maximum.((abs.(Rx), abs.(Ry), abs.(RP)))
                 # push!(iter_evo, iter / max(nx, ny))
                 append!(errs_evo, errs)
                 @printf(
@@ -280,14 +264,14 @@ function main(nt)
         end
         t += dt
         push!(evo_t, t)
-        push!(evo_τxx, maximum(Txx))
+        push!(evo_τxx, maximum(τxx))
         push!(iter_evo, iter-1)
 
         # visualisation
-        p1 = heatmap(xc, yc, Pt'   , title="Pressure"; plot_opts()...)
-        p3 = heatmap(xc, yc, TII'  , title="TII"     ; plot_opts()...)
-        p2 = heatmap(xc, yc, η_vep', title="η_vep"   ; plot_opts()...)
-        p4 = plot(evo_t,evo_τxx,legend=false,xlabel="time",ylabel="max(Txx)",linewidth=0,markershape=:circle,markersize=3,framestyle=:box)
+        p1 = heatmap(xc, yc, Array(Pt)'   , title="Pressure"; plot_opts()...)
+        p3 = heatmap(xc, yc, Array(TII)'  , title="TII"     ; plot_opts()...)
+        p2 = heatmap(xc, yc, Array(η_vep)', title="η_vep"   ; plot_opts()...)
+        p4 = plot(evo_t,evo_τxx,legend=false,xlabel="time",ylabel="max(τxx)",linewidth=0,markershape=:circle,markersize=3,framestyle=:box)
         display(plot(p1,p2,p3,p4,layout=(2,2)))
         # png(plot(p1,p2,p3,p4,layout=(2,2)),@sprintf("anim/%04d.png",iframe+=1))
     end
